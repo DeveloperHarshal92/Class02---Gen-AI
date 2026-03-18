057a10d77fbeaa2eaba7b5dd96dc69cfbbbad3e6
Harshal Varade <developer.ever@gmail.com>
2026-03-18 18:56:43 +0530

    Implementation of ai agent tools and a feature is created to send email to anyone via agent

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,18 @@
 import "dotenv/config";
 import readline from "readline/promises";
 import { ChatMistralAI } from "@langchain/mistralai";
-import { HumanMessage } from "langchain";
+import { createAgent, HumanMessage, tool } from "langchain";
+import { sendEmail } from "./mail.service.js";
+import * as z from "zod";
+const emailTool = tool(sendEmail, {
+  name: "emailTool",
+  description: "Use this tool to send an email",
+  schema: z.object({
+    to: z.string().describe("The reciptient's email address"),
+    html: z.string().describe("The HTML content of the email"),
+    subject: z.string().describe("The subject of the email"),
+  }),
+});
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -12,6 +23,11 @@ const model = new ChatMistralAI({
   model: "mistral-small-latest",
 });
 
+const agent = createAgent({
+  model,
+  tools: [emailTool],
+});
+
 const messages = [];
 
 while (true) {
@@ -19,9 +35,14 @@ while (true) {
 
   messages.push(new HumanMessage(userInput));
 
-  const response = await model.invoke(userInput);
+  const response = await agent.invoke({
+    messages,
+  });
 
-  console.log(`\x1b[34m[AI]:\x1b[0m ${response.content}`);
+  messages.push(response.messages[response.messages.length - 1]);
+
+  console.log(
+    `\x1b[34m[AI]\x1b[0m ${response.messages[response.messages.length - 1].content}`,
+  );
 }
 
-rl.close();
